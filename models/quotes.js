d67dca68d1e12fb23008fd51c1f5cba70e7c14ac
engdavide <twodoorbells@gmail.com>
2018-04-23 22:51:36 +0000

    add basic quote model and lines

--- models/quotes.js
+++ models/quotes.js
@@ -8,6 +8,7 @@ const   Users = require('../models/users'),
 let quotesSchema = new mongoose.Schema({
     quoteNum: Number,
     totalPrice: String,
+    panel: String,
     substrate: String,
     paint: String,
     color: String,
